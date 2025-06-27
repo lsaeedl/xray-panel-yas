65b37a9cd6c378a4f8b30553685d16f04a624dc0
lsaeedl <42690673+lsaeedl@users.noreply.github.com>
2025-06-27 05:58:55 +0330

    Update run.py

--- run.py
+++ run.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
 
 from flask_migrate import Migrate
-from app import app, db
+from app import app, db  # فرض اینکه app و db قبلاً تعریف شده‌اند
 
 migrate = Migrate(app, db)
+
 
